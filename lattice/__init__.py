4d74b2934cb0fd3d205e8eff2bb84ef883067217
SaltyChiang <SaltyChiang@users.noreply.github.com>
2023-04-04 00:35:33 +0800

    Use a new representation for insertion.

--- lattice/__init__.py
+++ lattice/__init__.py
@@ -2,4 +2,4 @@ from .backend import getBackend, setBackend
 from .dispatch import Dispatch, processBar
 from .elemental import ElementalGenerator
 from .preset import GaugeFieldTimeSlice, EigenVectorTimeSlice, PerambulatorBinary, GaugeFieldIldg, ElementalNpy, Jpsi2gammaNpy
-from .operator import gamma, derivative
+from .insertion import gamma, derivative
