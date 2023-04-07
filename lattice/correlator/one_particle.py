a341d92b91036de4eefd13c71e04352a2c201398
SaltyChiang <SaltyChiang@users.noreply.github.com>
2023-04-08 00:28:53 +0800

    Update twopt.py to fit the new API.

--- lattice/correlator/one_particle.py
+++ lattice/correlator/one_particle.py
@@ -8,22 +8,21 @@ from ..filedata.abstract import FileData
 from ..backend import getBackend
 
 
-def twopoint(operators: List[Operator], coefficients: List[float], elemental: FileData, perambulator: FileData, timeslices: Iterable[int]):
+def twopoint(operators: List[Operator], elemental: FileData, perambulator: FileData, timeslices: Iterable[int]):
     numpy = getBackend()
     Nop = len(operators)
-    Np = len(operators[0].momentum)
     Nt = len(timeslices)
 
-    ret = numpy.zeros((Nop, Np, Nt), "<c16")
-    phis = getElementalData(operators, coefficients, elemental)
+    ret = numpy.zeros((Nop, Nt), "<c16")
+    phis = getElementalData(operators, elemental)
     for t in timeslices:
         tau = perambulator[t]
         tau_bw = contract("ij,tkjba,kl->tilab", gamma(15), tau.conj(), gamma(15))
         for idx in range(Nop):
             phi = phis[idx]
             ret[idx] += contract(
-                "tijab,xjk,xptbc,tklcd,yli,ypad->pt", tau_bw, phi[0], numpy.roll(phi[1], -t, 1), tau, phi[0],
-                phi[1][:, :, t].conj()
+                "tijab,xjk,xtbc,tklcd,yli,yad->t", tau_bw, phi[0], numpy.roll(phi[1], -t, 1), tau, phi[0],
+                phi[1][:, t].conj()
             )
         print(f"{perambulator.sizeInByte/perambulator.timeInSec/1024**2} MB/s")
     ret /= Nt
