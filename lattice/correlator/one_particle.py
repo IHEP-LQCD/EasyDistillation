ec0ebaa5a355c69d4d1c06c675607a56273b6192
shichunjiang@ihep.ac.cn <shichunjiang@ln002.csns.ihep.ac.cn>
2023-04-10 21:30:36 +0800

    Add twopointMatrix(), twopointMom2().

--- lattice/correlator/one_particle.py
+++ lattice/correlator/one_particle.py
@@ -8,23 +8,51 @@ from ..filedata.abstract import FileData
 from ..backend import getBackend
 
 
-def twopoint(operators: List[Operator], elemental: FileData, perambulator: FileData, timeslices: Iterable[int]):
+def twopoint(
+    operators: List[Operator], elemental: FileData, perambulator: FileData, timeslices: Iterable[int], Lt: int
+):
     numpy = getBackend()
     Nop = len(operators)
     Nt = len(timeslices)
 
-    ret = numpy.zeros((Nop, Nt), "<c16")
+    ret = numpy.zeros((Nop, Lt), "<c16")
     phis = getElementalData(operators, elemental)
     for t in timeslices:
         tau = perambulator[t]
-        tau_bw = contract("ij,tkjba,kl->tilab", gamma(15), tau.conj(), gamma(15))
+        tau_bw = contract("ii,tjiba,jj->tijab", gamma(15), tau.conj(), gamma(15))
         for idx in range(Nop):
             phi = phis[idx]
             ret[idx] += contract(
                 "tijab,xjk,xtbc,tklcd,yli,yad->t", tau_bw, phi[0], numpy.roll(phi[1], -t, 1), tau, phi[0],
                 phi[1][:, t].conj()
             )
-        print(f"{perambulator.sizeInByte/perambulator.timeInSec/1024**2} MB/s")
+        print(f"t{t}: {perambulator.sizeInByte/perambulator.timeInSec/1024**2} MB/s")
     ret /= Nt
 
+    return ret
+
+
+def twopointMatrix(
+    operators: List[Operator], elemental: FileData, perambulator: FileData, timeslices: Iterable[int], Lt: int
+):
+    numpy = getBackend()
+    Nop = len(operators)
+    Nt = len(timeslices)
+
+    ret = numpy.zeros((Nop, Nop, Lt), "<c16")
+    phis = getElementalData(operators, elemental)
+    for t in timeslices:
+        tau = perambulator[t]
+        tau_bw = contract("ii,tjiba,jj->tijab", gamma(15), tau.conj(), gamma(15))
+        for isrc in range(Nop):
+            for isnk in range(Nop):
+                phi_src = phis[isrc]
+                gamma_src = contract("ij, xkj, kl -> xil", gamma(8), phi_src[0].conj(), gamma(8))
+                phi_snk = phis[isnk]
+                ret[isrc, isnk] += contract(
+                    "tijab,xjk,xtbc,tklcd,yli,yad->t", tau_bw, phi_snk[0], numpy.roll(phi_snk[1], -t, 1), tau,
+                    gamma_src, phi_src[1][:, t].conj()
+                )
+        print(f"t{t}: {perambulator.sizeInByte/perambulator.timeInSec/1024**2} MB/s")
+    ret /= Nt
     return ret
--- twopt.py
+++ twopt.py
@@ -6,7 +6,7 @@ cupy.cuda.Device(1).use()
 from lattice import preset, insertion
 from lattice.insertion import Insertion, Operator
 from lattice.insertion.mom_dict import momDict_mom9
-from lattice.correlator.one_particle import twopoint
+from lattice.correlator.one_particle import twopoint, twopointMatrix
 
 elemental = preset.ElementalNpy(
     "/dg_hpc/LQCD/shichunjiang/DATA/clqcd_nf2_clov_L16_T128_b2.0_ml-0.05862_sn2_srho0.12_gg5.65_gf5.2_usg0.780268_usf0.949104/04.meson.mom9/clqcd_nf2_clov_L16_T128_b2.0_xi5_ml-0.05862_cfg_",
@@ -21,10 +21,26 @@ insertionField = Insertion(
 )
 print(insertionField)
 operator_pi = Operator("pi", [insertionField[0](0, 0, 0)], [1])
-print(operator_pi.parts)
+print(operator_pi.name, operator_pi.parts)
 
 e = elemental.load("2000")
 p = perambulator.load("2000")
 numpy = getBackend()
-twopt = twopoint([operator_pi], e, p, list(range(128))).reshape(128)
+# compute 2pt
+twopt = twopoint([operator_pi], e, p, list(range(128)), 128).reshape(128)
 print(numpy.arccosh((numpy.roll(twopt, -1, 0) + numpy.roll(twopt, 1, 0)) / twopt / 2))
+
+# compute a 2 by 2 two-point correlation matrix
+insertionField2 = Insertion(
+    insertion.GAMMA_NAME.B1, insertion.DERIVATIVE_NAME.NABLA, insertion.PROJECTION_NAME.A1, momDict_mom9
+)
+operator_pi2 = Operator("pi2", [insertionField2[0](0, 0, 0)], [1])
+print(operator_pi2.name, operator_pi2.parts)
+twoptMatrix = twopointMatrix([operator_pi, operator_pi2], e, p, list(range(128)[::4]), 128)
+print(numpy.arccosh((numpy.roll(twoptMatrix[0,0], -1, 0) + numpy.roll(twoptMatrix[0,0], 1, 0)) / twoptMatrix[0,0] / 2))
+print(numpy.arccosh((numpy.roll(twoptMatrix[1,1], -1, 0) + numpy.roll(twoptMatrix[1,1], 1, 0)) / twoptMatrix[1,1] / 2))
+
+# compute summation of p2 = 1 2pt
+from lattice.correlator.disperion_relation import twopointMom2
+twoptMom2 = twopointMom2(insertionField[0], 2, e, p, list(range(128)), 128)
+print(twoptMom2)
