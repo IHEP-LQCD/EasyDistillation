ecab8b5b0e4a391ced708fd09768e62e7b5a8410
SaltyChiang <SaltyChiang@users.noreply.github.com>
2023-04-11 18:23:41 +0800

    Some advices.

---
+++ TODO.md
@@ -0,0 +1,13 @@
+#
+
+Use group theory to do the product.
+
+2-particle correlation function.
+
+P, D, ... wave.
+
+Integrate PyQuda to calculate sequencial source.
+
+3-point function.
+
+<del>Helicity operator.</del>
--- gen_twopt.py
+++ gen_twopt.py
@@ -1,12 +1,23 @@
 import cupy
-
-from lattice.backend import setBackend, getBackend
+from lattice import setBackend, getBackend
 setBackend(cupy)
-# cupy.cuda.Device(1).use()
-from lattice import preset, Dispatch
-from lattice.insertion import Insertion, Operator, GAMMA_NAME, DERIVATIVE_NAME, PROJECTION_NAME
+cupy.cuda.Device(1).use()
+
+###############################################################################
 from lattice.insertion.mom_dict import momDict_mom9
-from lattice.correlator.one_particle import twopoint, twopointMatrix
+from lattice.insertion import Insertion, Operator, GAMMA_NAME, DERIVATIVE_NAME, PROJECTION_NAME
+
+pi_A1 = Insertion(GAMMA_NAME.PI, DERIVATIVE_NAME.IDEN, PROJECTION_NAME.A1, momDict_mom9)
+print(pi_A1[0])
+op_pi = Operator("pi", [pi_A1[0](0, 0, 0)], [1])
+
+b1xnabla_A1 = Insertion(GAMMA_NAME.B1, DERIVATIVE_NAME.NABLA, PROJECTION_NAME.A1, momDict_mom9)
+print(b1xnabla_A1[0])
+op_pi2 = Operator("pi2", [pi_A1[0](0, 0, 0), b1xnabla_A1[0](0, 0, 0)], [3, 1])
+###############################################################################
+
+###############################################################################
+from lattice import preset
 
 elemental = preset.ElementalNpy(
     "/dg_hpc/LQCD/shichunjiang/DATA/clqcd_nf2_clov_L16_T128_b2.0_ml-0.05862_sn2_srho0.12_gg5.65_gf5.2_usg0.780268_usf0.949104/04.meson.mom9/clqcd_nf2_clov_L16_T128_b2.0_xi5_ml-0.05862_cfg_",
@@ -16,32 +27,29 @@ perambulator = preset.PerambulatorNpy(
     "/dg_hpc/LQCD/shichunjiang/DATA/clqcd_nf2_clov_L16_T128_b2.0_ml-0.05862_sn2_srho0.12_gg5.65_gf5.2_usg0.780268_usf0.949104/03.perambulator.light.single.prec1e-9/clqcd_nf2_clov_L16_T128_b2.0_xi5_ml-0.05862_cfg_",
     ".peram.npy", [128, 128, 4, 4, 70, 70], 70
 )
-insertionField = Insertion(GAMMA_NAME.PI, DERIVATIVE_NAME.IDEN, PROJECTION_NAME.A1, momDict_mom9)
-print(insertionField[0])
-operator_pi = Operator("pi", [insertionField[0](0, 0, 0)], [1])
 
-insertionField2 = Insertion(GAMMA_NAME.B1, DERIVATIVE_NAME.NABLA, PROJECTION_NAME.A1, momDict_mom9)
-print(insertionField2[0])
-operator_pi2 = Operator("pi2", [insertionField2[0](0, 0, 0)], [1])
+cfg = "2000"
+e = elemental.load(cfg)
+p = perambulator.load(cfg)
+###############################################################################
 
+###############################################################################
+from lattice.correlator.one_particle import twopoint, twopointMatrix
 np = getBackend()
-twopt = np.zeros((2, 128), "<c16")
-dispatcher = Dispatch("cfglist.full.txt", "aaa")
-for cfg in dispatcher:
-    e = elemental.load(cfg)
-    p = perambulator.load(cfg)
-    # compute 2pt
-    twopt += twopoint([operator_pi, operator_pi2], e, p, list(range(128)), 128)
+
+# compute 2pt
+twopt = twopoint([op_pi, op_pi2], e, p, list(range(128)), 128)  # [Nop, Lt]
 twopt = twopt.real
 print(np.arccosh((np.roll(twopt, -1, 1) + np.roll(twopt, 1, 1)) / twopt / 2))
 
 # compute a 2 by 2 two-point correlation matrix
-twoptMatrix = twopointMatrix([operator_pi, operator_pi2], e, p, list(range(128)), 128)
+twoptMatrix = twopointMatrix([op_pi, op_pi2], e, p, list(range(128)), 128)
 twoptMatrix = twoptMatrix.real
 print(np.arccosh((np.roll(twoptMatrix[0, 0], -1, 0) + np.roll(twoptMatrix[0, 0], 1, 0)) / twoptMatrix[0, 0] / 2))
 print(np.arccosh((np.roll(twoptMatrix[1, 1], -1, 0) + np.roll(twoptMatrix[1, 1], 1, 0)) / twoptMatrix[1, 1] / 2))
+###############################################################################
 
 # compute summation of p2 = 1 2pt
 from lattice.correlator.disperion_relation import twopointMom2
-twoptMom2 = twopointMom2(insertionField[0], 2, e, p, list(range(128)), 128)
+twoptMom2 = twopointMom2(pi_A1[0], 2, e, p, list(range(128)), 128)
 print(twoptMom2)
