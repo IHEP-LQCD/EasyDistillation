e8ca048b0e6aaa1a8fb4e6eb2fff9fc6c07fe3b6
Chunjiang-Shi <shichunjiang16@mails.ucas.ac.cn>
2023-05-15 17:35:42 +0800

    Add get_AB_opratorlist_rows.py .

--- lattice/correlator/two_particles.py
+++ lattice/correlator/two_particles.py
@@ -3,6 +3,7 @@ from typing import List, Tuple
 
 from ..insertion import InsertionRow, Operator
 
+
 def get_mom2_list(mom2: int) -> List[Tuple]:
     ret = []
     for i in product(range(-3, 4), repeat=3):
@@ -13,11 +14,27 @@ def get_mom2_list(mom2: int) -> List[Tuple]:
     return ret
 
 
-def get_AB_opratorList_back2back(insertion_row_A: InsertionRow, insertion_row_B: InsertionRow, mom_list:List[Tuple]) -> List[Operator]:
+def get_AB_opratorlist_row(insertion_row_A: InsertionRow, insertion_row_B: InsertionRow,
+                                 mom_list: List[Tuple]) -> List[Operator]:
     ops_A = []
     ops_B = []
     for i in mom_list:
         px, py, pz = i
-        ops_A.append(Operator("",[insertion_row_A(px, py, pz)], [1]))
-        ops_B.append(Operator("",[insertion_row_B(-px, -py, -pz)], [1]))
+        ops_A.append(Operator("", [insertion_row_A(px, py, pz)], [1]))
+        ops_B.append(Operator("", [insertion_row_B(-px, -py, -pz)], [1]))
+    return ops_A, ops_B
+
+
+def get_AB_opratorlist_rows(
+    insertions_row_A: List[InsertionRow], insertions_row_B: List[InsertionRow], mom_list: List[Tuple], coeff: List = None
+) -> List[Operator]:
+    assert len(insertions_row_A) == len(insertions_row_B)
+    if coeff is None:
+        coeff = [1] * len(insertions_row_A)
+    ops_A = []
+    ops_B = []
+    for i in mom_list:
+        px, py, pz = i
+        ops_A.append(Operator("", [i(px, py, pz) for i in insertions_row_A], coeff))
+        ops_B.append(Operator("", [i(-px, -py, -pz) for i in insertions_row_B], coeff))
     return ops_A, ops_B
