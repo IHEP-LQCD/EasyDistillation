4e16c5f3666bda17b7a571d34ed707b2306d8df7
Chunjiang-Shi <shichunjiang16@mails.ucas.ac.cn>
2023-08-14 23:58:57 +0800

    Add `usedNe` < `Ne` support.

--- lattice/data.py
+++ lattice/data.py
@@ -4,7 +4,7 @@ from .insertion import Operator
 from .filedata.abstract import FileData
 
 
-def get_elemental_data(operators: List[Operator], elemental: FileData):
+def get_elemental_data(operators: List[Operator], elemental: FileData, usedNe: int):
     from .backend import get_backend
     from .insertion.gamma import gamma
 
@@ -23,7 +23,7 @@ def get_elemental_data(operators: List[Operator], elemental: FileData):
                 elemental_coeff, derivative_idx, momentum_idx = elemental_part[j]
                 deriv_mom_tuple = (derivative_idx, momentum_idx)
                 if deriv_mom_tuple not in cache:
-                    cache[deriv_mom_tuple] = elemental[derivative_idx, momentum_idx]
+                    cache[deriv_mom_tuple] = elemental[derivative_idx, momentum_idx, :, :usedNe, :usedNe]
                 if j == 0:
                     ret_elemental.append(elemental_coeff * cache[deriv_mom_tuple])
                 else:
